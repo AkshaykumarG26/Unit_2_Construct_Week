05a0b5f89980978a84fa8ab830ee640a8e005669
Akshaykumar26 <akshayg2697@gmail.com>
2021-10-01 11:50:29 +0530

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-    let products = [
+    let trendingProducts = [
         {
             name: "EASY BATH WET WIPES",
             disc: "pacet 10 wet wipes",
@@ -44,26 +44,71 @@
         }
     ]
     
+    let recommendedProducts = [
+        {
+            name: "TRESSGRO LOTION",
+            disc: "bottle of 100ml Lotion",
+            price: "₹ 722.90",
+            img: "https://mednear.com/uploads/icons/bottle.png"
+        },
+        {
+            name: "MANFORCE 50MG TABLET",
+            disc: "strip of 9 Tablets",
+            price: "₹ 197",
+            img: "https://mednear.com/uploads/medicine/2f7e8584b66331a6858a373eb8c22afc.jpg"
+        },
+        {
+            name: "MANFORCE 100MG TABLET",
+            disc: "strip of 4 Tablets",
+            price: "₹ 229",
+            img: "https://mednear.com/uploads/medicine/7332ee0350d09046239b80bd4e290120.jpg"
+        },
+        {   
+            name: "MFB AL SHIFA CHURAN PACK OF 3",
+            disc: "bottle of 100 gm Churna",
+            price: "₹ 341",
+            img: "https://mednear.com/uploads/medicine/7fc7425c2329e03670a347e47d3c4455.png"
+        },
+        {
+            name: "CASTOR NF CREAM",
+            disc: "tube of 15gm Cream",
+            price: "₹ 84.15",
+            img: "https://mednear.com/uploads/icons/drugs.png"
+        },
+        {
+            name: "NUTRIHUB GOLD SOFTGEL",
+            disc: "bottle of 30 softgel capsules",
+            price: "₹ 1995",
+            img: "https://mednear.com/uploads/medicine/9746a65404b16f1d5e4662deea0b66d6.png"
+        },
+        {
+            name: "DANGO SHAMPOO",
+            disc: 'bottle of 100ml Shampoo',
+            price: "₹ 181",
+            img: "https://mednear.com/uploads/medicine/ecb71805b859e3ac8903d06c5c9938a3.jpg"   
+        }
+    ]
     
-    let parent = document.getElementById("trendingProducts");
+    let trendingParent = document.getElementById("trendingProducts");
+    let recommendedParent = document.getElementById("recommendedProducts")
     
     function showProducts(){
     
-        products.forEach(function(product){
+        trendingProducts.forEach(function(product){
     
             let div = document.createElement("div");
             let img = document.createElement("img");
     
             img.src = product.img;
     
-            let p_name = document.createElement("p");
-            p_name.textContent = product.name;
+            let tp_name = document.createElement("p");
+            tp_name.textContent = product.name;
     
-            let p_price = document.createElement("p");
-            p_price.textContent = product.price;
+            let tp_price = document.createElement("p");
+            tp_price.textContent = product.price;
     
-            let p_disc = document.createElement("p");
-            p_disc.textContent = product.disc;
+            let tp_disc = document.createElement("p");
+            tp_disc.textContent = product.disc;
     
             let add_cart_btn = document.createElement("button");
             add_cart_btn.textContent = "Add To Cart";
@@ -72,26 +117,50 @@
                 addToCart(product)
             }
     
-            div.append(img, p_name, p_disc, p_price,  add_cart_btn);
-            parent.append(div)
-    
-    
+            div.append(img, tp_name, tp_disc, tp_price,  add_cart_btn);
+            trendingParent.append(div)
         })
     
+        recommendedProducts.forEach(function(product){
+    
+            let div = document.createElement("div");
+            let img = document.createElement("img");
+    
+            img.src = product.img;
+    
+            let tp_name = document.createElement("p");
+            tp_name.textContent = product.name;
+    
+            let tp_price = document.createElement("p");
+            tp_price.textContent = product.price;
+    
+            let tp_disc = document.createElement("p");
+            tp_disc.textContent = product.disc;
+    
+            let add_cart_btn = document.createElement("button");
+            add_cart_btn.textContent = "Add To Cart";
+    
+            add_cart_btn.onclick = function(){
+                addToCart(product)
+            }
+    
+            div.append(img, tp_name, tp_disc, tp_price,  add_cart_btn);
+            recommendedParent.append(div)
+        })
     }
     
     showProducts();
     
     
     
-    if (localStorage.getItem("mednearCart") === null){
-        localStorage.setItem("mednearCart", JSON.stringify([]));
-    }
+    // if (localStorage.getItem("mednearCart") === null){
+    //     localStorage.setItem("mednearCart", JSON.stringify([]));
+    // }
     
-    function addToCart(prod){
-        let products_in_cart = JSON.parse(localStorage.getItem("mednearCart"));
-        products_in_cart.push(prod);
-        localStorage.setItem("mednearCart", JSON.stringify(products_in_cart));
-    }
+    // function addToCart(prod){
+    //     let products_in_cart = JSON.parse(localStorage.getItem("mednearCart"));
+    //     products_in_cart.push(prod);
+    //     localStorage.setItem("mednearCart", JSON.stringify(products_in_cart));
+    // }
     
   
